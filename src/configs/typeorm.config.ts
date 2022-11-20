9c6c7ced6fdc68bf88a2c62287687887b07f9ba5
rjsdn0124 <201802145@o.cnu.ac.kr>
2022-11-20 22:05:58 +0900

    :twisted_rightwards_arrows: merging

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -4,11 +4,11 @@ import { User } from 'src/User/user.entity';
 
 export const typeORMConfig: TypeOrmModuleOptions = {
   type: 'mysql',
-  host: '127.0.0.1',
+  host: 'localhost',
   port: 3306,
   username: 'root',
-  password: '1234',
+  password: 'Qwer1234!@',
   database: 'DoNotRoughly',
   entities: [User, Course],
-  synchronize: false,
+  synchronize: true,
 };
--- src/course/course.module.ts
+++ src/course/course.module.ts
@@ -4,10 +4,17 @@ import { CourseService } from './course.service';
 import { Course } from './course.entity';
 import { CourseRepository } from './course.repository';
 import { TypeOrmExModule } from 'src/typeorm/typeorm-custom-repository.module';
+import { UserRepository } from 'src/user/user.repository';
 
 @Module({
-  imports: [TypeOrmExModule.forCustomRepository([Course, CourseRepository])],
+  imports: [
+    TypeOrmExModule.forCustomRepository([
+      Course,
+      CourseRepository,
+      UserRepository,
+    ]),
+  ],
   controllers: [CourseController],
-  providers: [CourseService],
+  providers: [CourseService, CourseService],
 })
 export class CourseModule {}
