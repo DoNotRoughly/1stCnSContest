fc0d3f51658feab3b7f241861a45df03c04af1eb
Geonu Lim <44187187+rjsdn0124@users.noreply.github.com>
2022-11-20 19:23:44 +0900

    user login과 course 연동을 위한 pr (#4)
    * :construction: 작업즁
    
    * :bug: db 연결하고 스키마 조금 변경했음
    
    그리고 문제점은 typeorm custom repository!
    
    * :sparkles: user api 임시 마무리!

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,30 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Patch, Post, Req, Res } from '@nestjs/common';
+import { Request, Response } from 'express';
+import { identity } from 'rxjs';
 import { UserService } from './user.service';
 
-@Controller()
+@Controller('user')
 export class UserController {
+  // user api router
   constructor(private readonly userService: UserService) {}
 
-  @Get()
-  getHello(): string {
-    return this.userService.getHello();
+  @Post('login')
+  async login(@Req() req: Request, @Res() res: Response) {
+    // 로그인 api
+    const result = await this.userService.loginInfo(
+      req.body.userId,
+      req.body.pw,
+    );
+    return res.status(result.status).json(result);
+  }
+
+  @Patch('apply')
+  async apply(@Req() req: Request, @Res() res: Response) {
+    // 수강신청 api
+    const result = await this.userService.applyCourse(
+      req.body.userId,
+      req.body.courseId,
+    );
+    return res.status(result.status).json(result);
   }
 }
