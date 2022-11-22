2ba996d019d0fdbe69f714fe779278fb59c55566
rjsdn0124 <201802145@o.cnu.ac.kr>
2022-11-23 03:10:12 +0900

    :twisted_rightwards_arrows: merge 실수 처리.

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-<<<<<<< HEAD
   app.enableCors();
   app.setGlobalPrefix('api');
-=======
->>>>>>> af119f6 (:twisted_rightwards_arrows: merging)
   await app.listen(3000);
 }
 bootstrap();
