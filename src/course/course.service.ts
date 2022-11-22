aa366470d761aaedc1ca0708b34392559f8794bb
rjsdn0124 <201802145@o.cnu.ac.kr>
2022-11-23 00:41:13 +0900

    :sparkles: 강의 삭제됨미다.

--- src/course/course.service.ts
+++ src/course/course.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/user/user.entity';
 import { DataSource } from 'typeorm';
 import { Course } from './course.entity';
 import { CourseRepository } from './course.repository';
@@ -64,10 +65,21 @@ export class CourseService {
       return { status: 400, message: '오류가 발생했습니다.' };
     }
   }
-
+  async sendEmail(user_list: User[]) {
+    return user_list;
+  }
   async deleteCourse(courseId: string) {
-    const result = await this.courseRepository.delete(courseId);
-    return { status: 201, result };
+    try {
+      const course = await this.courseRepository.findUserListByCourse(courseId);
+      this.sendEmail(course[0].user);
+      const result = await this.courseRepository.delete(courseId);
+      if (!result.affected) {
+        return { status: 400, message: '삭제할 수 없는 강의입니다.' };
+      }
+      return { status: 201, result };
+    } catch (e) {
+      return { status: 400, message: '오류가 발생했습니다.' };
+    }
   }
 
   // 필터링 하여 강의 목록 반환
