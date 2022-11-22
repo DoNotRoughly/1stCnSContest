729b5dd39aaf600ecf45b542f3a86a75b44ecfbf
rjsdn0124 <201802145@o.cnu.ac.kr>
2022-11-23 02:41:53 +0900

    :rewind: 이메일 그냥 잠궜습니다.

--- src/course/course.service.ts
+++ src/course/course.service.ts
@@ -1,7 +1,7 @@
+import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/user/user.entity';
-import { DataSource } from 'typeorm';
 import { Course } from './course.entity';
 import { CourseRepository } from './course.repository';
 import { CourseReturn } from './course.returns';
@@ -15,7 +15,7 @@ export class CourseService {
     //생성자
     @InjectRepository(CourseRepository)
     private readonly courseRepository: CourseRepository,
-    private dataSource: DataSource,
+    private mailerService: MailerService,
   ) {
     this.resentlabel = 'major';
     this.resentvalue = '';
@@ -57,7 +57,7 @@ export class CourseService {
       if (alreadyIn) {
         return { status: 400, message: '이미 등록되어 있는 강의입니다.' };
       }
-      await this.courseRepository.save(course);
+      // await this.courseRepository.save(course);
       const result = await this.returnCourseList(await this.filter('', ''));
       console.log(course);
       return { status: 201, result };
@@ -68,13 +68,30 @@ export class CourseService {
   }
 
   async sendEmail(user_list: User[]) {
-    return user_list;
+    // 이메일 보내줘요잉
+    try {
+      for (const user of user_list) {
+        const email = user.email;
+        console.log(email);
+        await this.mailerService.sendMail({
+          to: email, // list of receivers
+          from: 'DoNotRoughly@naver.com', // sender address
+          subject: '강의가 취소되었습니다..', // Subject line
+          html: '취소됨요..', //취소메시지
+        });
+      }
+      console.log('email 전송을 완료했습니다.');
+    } catch (err) {
+      console.log(err);
+    }
+    return;
   }
-  
+
   async deleteCourse(courseId: string) {
+    // 삭제
     try {
       const course = await this.courseRepository.findUserListByCourse(courseId);
-      this.sendEmail(course[0].user);
+      await this.sendEmail(course[0].user);
       const isDeleted = await this.courseRepository.delete(courseId);
       if (!isDeleted.affected) {
         return { status: 400, message: '삭제할 수 없는 강의입니다.' };
@@ -84,7 +101,6 @@ export class CourseService {
     } catch (e) {
       return { status: 400, message: '오류가 발생했습니다.' };
     }
-
   }
 
   // 필터링 하여 강의 목록 반환
